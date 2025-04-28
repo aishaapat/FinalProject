c4f50524ab357e292e91aa0e0a2e8e08f51991f5
Avb2 <bringuel.alexander@gmail.com>
2025-04-28 19:28:32 -0400

    added MapPoint init

--- finalProject/MapPoint.swift
+++ finalProject/MapPoint.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import MapKit
 
 
 class MapPoint: NSObject, MKAnnotation {
@@ -14,5 +14,13 @@ class MapPoint: NSObject, MKAnnotation {
     var photo: String?
     var longitude: Double
     var latitude: Double
-
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    init (longitude: Double, latitude: Double){
+        self.longitude = longitude
+        self.latitude = latitude
+    }
 }
