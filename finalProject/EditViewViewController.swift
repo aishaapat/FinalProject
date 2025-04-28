4fd0a9e960636eb8ddc915c00a47798d98dc51ea
Avb2 <bringuel.alexander@gmail.com>
2025-04-28 16:13:49 -0400

    first commit

--- finalProject/EditViewViewController.swift
+++ finalProject/EditViewViewController.swift
@@ -9,6 +9,49 @@ import UIKit
 
 class EditViewViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var latitudeField: UITextField!
+    @IBOutlet weak var longitudeField: UITextField!
+    @IBOutlet weak var viewEditSwitch: UISegmentedControl!
+    
+    
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet weak var takePhotoButton: UIButton!
+    
+    
+    
+    @IBAction func changeEditMode(_ sender: UISegmentedControl) {
+        let textFields: [UITextField] = [nameField, latitudeField, longitudeField]
+        
+        let buttons: [UIButton] = [
+            selectButton,
+            takePhotoButton
+        ]
+        
+        if viewEditSwitch.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            for button in buttons {
+                button.isHidden = true
+                button.isEnabled = false
+            }
+        } else if viewEditSwitch.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            for button in buttons {
+                button.isHidden = false
+                button.isEnabled = true
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
