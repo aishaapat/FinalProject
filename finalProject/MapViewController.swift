89d6b0797c44b5ee9f1b2d2207c7f5fc24769500
aishaapat <aishapatel0703@gmail.com>
2025-04-29 17:30:03 -0400

    merge deleted all my work I added it back

--- finalProject/MapViewController.swift
+++ finalProject/MapViewController.swift
@@ -1,18 +1,16 @@
-//
-//  MapViewController.swift
-//  finalProject
-//
-//  Created by Alex Bringuel on 4/28/25.
-//
-
 import UIKit
+import CoreLocation
 import MapKit
+import CoreData
 
-class MapViewController: UIViewController {
-
-    @IBOutlet weak var mapView: MKMapView!
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
+    var locationManager: CLLocationManager!
+    var addresses: [Address] = []
+    
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var switchMapType: UISegmentedControl!
+    
     @IBAction func mapChangeType(_ sender: UISegmentedControl) {
         switch switchMapType.selectedSegmentIndex {
         case 0:
@@ -25,16 +23,49 @@ class MapViewController: UIViewController {
             break
         }
     }
-    //adding comments
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
         mapView.setUserTrackingMode(.follow, animated: true)
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        // Fetch Address objects from Core Data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
 
+        let fetchRequest: NSFetchRequest<Address> = Address.fetchRequest()
+        do {
+            addresses = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Failed to fetch Address: \(error), \(error.userInfo)")
+        }
+
+        mapView.removeAnnotations(mapView.annotations)
+
+        for address in addresses {
+            if let latitutde = address.latitude,
+               let longititude = address.longititude,
+               let lat = Double(latitutde),
+               let lon = Double(longititude) {
+                
+                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
+                annotation.title = address.name ?? "N/A"
+                annotation.subtitle = "Lat: \(lat), Lon: \(lon)"
+                
+                mapView.addAnnotation(annotation)
+            }
+        }
+    }
 }
+
