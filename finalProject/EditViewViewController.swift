af766cc8f930e8def75e7475627f51599b9b6613
aishaapat <aishapatel0703@gmail.com>
2025-04-29 16:43:19 -0400

    added methods to save and sned textfields to the db and also save the photo captured to the db

--- finalProject/EditViewViewController.swift
+++ finalProject/EditViewViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class EditViewViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-
+    
+    var currentAddress : Address?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var imageData: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
@@ -17,23 +22,30 @@ class EditViewViewController: UIViewController, UIImagePickerControllerDelegate 
     @IBOutlet weak var longitudeField: UITextField!
     @IBOutlet weak var viewEditSwitch: UISegmentedControl!
     
+    @IBOutlet weak var saveBtn: UIButton!
     
+    @IBAction func buttonSave(_ sender: Any)
+    {
+        appDelegate.saveContext()
+        viewEditSwitch.selectedSegmentIndex = 1
+        changeEditMode(viewEditSwitch)
+    }
     @IBOutlet weak var selectButton: UIButton!
     @IBOutlet weak var takePhotoButton: UIButton!
     
     
     @IBAction func openCameraRoll(_ sender: UIButton) {
         let picker = UIImagePickerController()
-           picker.sourceType = .photoLibrary
-           picker.delegate = self
-           present(picker, animated: true, completion: nil)
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
     }
     
     @IBAction func openCamera(_ sender: UIButton) {
         let picker = UIImagePickerController()
-               picker.sourceType = .camera
-               picker.delegate = self
-               present(picker, animated: true, completion: nil)
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
     }
     
     @IBAction func changeEditMode(_ sender: UISegmentedControl) {
@@ -64,22 +76,77 @@ class EditViewViewController: UIViewController, UIImagePickerControllerDelegate 
             }
         }
     }
+    @objc func textFieldDidEndEditing(_ textField: UITextField) {
+        if currentAddress == nil {
+            let context = appDelegate.persistentContainer.viewContext
+            currentAddress = Address(context: context)
+        }
+        
+        switch textField {
+        case nameField:
+            currentAddress?.name = textField.text
+        case latitudeField:
+            currentAddress?.latitude = textField.text
+        case longitudeField:
+            currentAddress?.longititude = textField.text
+        default:
+            break
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let textFields: [UITextField] = [nameField, longitudeField, latitudeField]
+        for textfield in textFields {
+            textfield.addTarget(self, action: #selector(self.textFieldDidEndEditing(_:)), for: .editingDidEnd)
+        }
 
-        // Do any additional setup after loading the view.
+        if currentAddress != nil {
+            nameField.text = currentAddress!.name
+            latitudeField.text = currentAddress!.latitude
+            longitudeField.text = currentAddress!.longititude
+            if let imageDataStored = currentAddress?.image {
+                imageData.image = UIImage(data: imageDataStored)
+            }
+        }
+
+        changeEditMode(viewEditSwitch)
+    }
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imageData.contentMode = .scaleAspectFit
+            if currentAddress == nil {
+                let context = appDelegate.persistentContainer.viewContext
+                currentAddress = Address(context: context)
+            }
+            currentAddress?.image = image.jpegData(compressionQuality: 0.8)
+            imageData.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
-}
+        
+ 
+        
+        
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
+
